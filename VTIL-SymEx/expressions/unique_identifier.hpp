ff55a2f973993f03d55cef63e03fafe23644fa23
Can Bölük <can.boluk89@gmail.com>
2020-04-29 04:50:29 +0200

    Implemented default hasher.

--- VTIL-SymEx/expressions/unique_identifier.hpp
+++ VTIL-SymEx/expressions/unique_identifier.hpp
@@ -109,7 +109,7 @@ namespace vtil::symbolic
 
 		// Construct from any other type.
 		//
-		template<typename T, typename hasher_t = hasher<T>,
+		template<typename T, typename hasher_t = hasher<>,
 			// Must not be an array or [const unique_identifier&].
 			std::enable_if_t<!std::is_same_v<T, unique_identifier> && !std::extent_v<T>, int> = 0>
 			unique_identifier( const T& v, std::string&& name = "" )
