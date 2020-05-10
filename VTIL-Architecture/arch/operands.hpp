c38f31c7fb9746af109e048f5583bcafed0f3468
Can Bölük <can.boluk89@gmail.com>
2020-05-10 06:31:25 +0200

    Should be the final fix for Clang.

--- VTIL-Architecture/arch/operands.hpp
+++ VTIL-Architecture/arch/operands.hpp
@@ -69,6 +69,16 @@ namespace vtil
 			// Declare reduction.
 			//
 			auto reduce() { return reference_as_tuple( u64, bit_count ); }
+
+			// TODO: Remove me.
+			//  Let modern compilers know that we use these operators as is,
+			//  implementation considering all candidates would be preferred
+			//  but since not all of our target compilers implement complete
+			//  ISO C++20, we have to go with this "patch".
+			//
+			using reducable::operator<;
+			using reducable::operator==;
+			using reducable::operator!=;
 		};
 
 		// Descriptor of this operand.
