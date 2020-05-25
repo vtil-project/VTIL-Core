bb5f998b6d83592178e7418d731aa865ea452575
Can Bölük <can.boluk89@gmail.com>
2020-05-25 15:27:27 +0200

    Fixed for Clang.

--- VTIL-Architecture/routine/basic_block.cpp
+++ VTIL-Architecture/routine/basic_block.cpp
@@ -99,9 +99,9 @@ namespace vtil
 	basic_block* basic_block::label_end()
 	{
 		auto [n, vip] = label_stack.back();
-		std::for_each( std::next( stream.begin(), n ), stream.end(), [ = ] ( instruction& ins )
+		std::for_each( std::next( stream.begin(), n ), stream.end(), [ v = vip_t( vip ) ]( instruction& ins )
 		{
-			ins.vip = ins.vip == invalid_vip ? vip : ins.vip;
+			ins.vip = ins.vip == invalid_vip ? v : ins.vip;
 		} );
 		label_stack.pop_back();
 		return this;
