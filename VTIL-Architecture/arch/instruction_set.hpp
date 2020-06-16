d9275285a2cea648c8f654e03581b3207534498f
Can Bölük <can.boluk89@gmail.com>
2020-06-16 20:21:15 +0200

    Fixed typo.

--- VTIL-Architecture/arch/instruction_set.hpp
+++ VTIL-Architecture/arch/instruction_set.hpp
@@ -142,7 +142,7 @@ namespace vtil
     
         //  -- Control flow instructions
         //                                                            
-        //    JS         Reg,    Reg/Imm,    Reg/Imm                        | Jumps to OP1 is set, jumps to OP2, otherwise OP3, continues virtual execution
+        //    JS         Reg,    Reg/Imm,    Reg/Imm                        | Jumps to OP1 ? OP2 : OP3, continues virtual execution
         //    JMP        Reg/Imm                                            | Jumps to OP1, continues virtual execution
         //    VEXIT      Reg/Imm                                            | Jumps to OP1, continues real execution
         //    VXCALL     Reg/Imm                                            | Calls into OP1, pauses virtual execution until the call returns
