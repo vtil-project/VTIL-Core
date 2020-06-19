9b9ccc1e3a0aa17d3366187755bfea2ddef82f92
Can Bölük <can.boluk89@gmail.com>
2020-06-20 00:22:33 +0200

    Removed unnecessary pragma once.

--- VTIL-SymEx/simplifier/boolean_directives.cpp
+++ VTIL-SymEx/simplifier/boolean_directives.cpp
@@ -25,7 +25,6 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
 // POSSIBILITY OF SUCH DAMAGE.        
 //
-#pragma once
 #include "boolean_directives.hpp"
 
 namespace vtil::symbolic::directive
