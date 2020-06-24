224eca7a4e4699b6df3f36dac8961c5ff1e88eff
Can Bölük <can.boluk89@gmail.com>
2020-06-25 01:13:51 +0200

    Changed Thread ID to ignore bottom 3 bits on Windows.

--- VTIL-Common/util/critical_section.cpp
+++ VTIL-Common/util/critical_section.cpp
@@ -45,7 +45,7 @@ namespace vtil
 	{
 #if _WIN64
 		static_assert( sizeof( tid_t ) == 8, "Thread identifier must be defined as a quadword." );
-		return __readgsqword( 0x48 );
+		return __readgsqword( 0x48 ) >> 3;
 #else
 		return ( tid_t ) syscall( SYS_gettid );
 #endif
