67958c909a1484adfc983337a88510a8826e6c38
Can Bölük <can.boluk89@gmail.com>
2020-06-25 03:34:05 +0200

    Changed variant size to fit a register descriptor.

--- VTIL-Common/util/variant.hpp
+++ VTIL-Common/util/variant.hpp
@@ -54,7 +54,7 @@
 // Determine the maximum size of types we should inline.
 //
 #ifndef VTIL_VARIANT_INLINE_LIMIT
-	#define VTIL_VARIANT_INLINE_LIMIT 16
+	#define VTIL_VARIANT_INLINE_LIMIT 32
 #endif
 
 namespace vtil
