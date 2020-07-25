f1dec2502a52e9369f127743294efb117a052830
Can Bölük <can.boluk89@gmail.com>
2020-07-25 23:26:09 +0200

    Implemented atomic detached queue.

--- VTIL-Common/util/detached_queue.hpp
+++ VTIL-Common/util/detached_queue.hpp
@@ -26,15 +26,18 @@
 // POSSIBILITY OF SUCH DAMAGE.        
 //
 #pragma once
-#include "../util/type_helpers.hpp"
+#include <atomic>
+#include <mutex>
+#include "intrinsics.hpp"
+#include "type_helpers.hpp"
 
 namespace vtil
 {
 	// Detached in-place queue for tracking already allocated objects 
 	// in a different order with no allocations.
 	//
-	template<typename T>
-	struct detached_queue
+	template<typename T, bool atomic>
+	struct base_detached_queue
 	{
 		// Detached key.
 		//
@@ -47,80 +50,116 @@ namespace vtil
 			const T* get( member_reference_t<T, key> ref ) const { return make_mutable( this )->get( std::move( ref ) ); }
 		};
 
-		// Head and tail for tracking the list.
+		// Spinlock protecting the list.
+		//
+		mutable std::atomic_flag spinlock = ATOMIC_FLAG_INIT;
+
+		// Head, tail and size tracking the list.
 		//
 		key* head = nullptr;
 		key* tail = nullptr;
+		size_t size = 0;
+
+		// Controls the lock.
+		//
+		void lock() const
+		{
+			if constexpr ( atomic )
+				while ( spinlock.test_and_set( std::memory_order_acquire ) )
+					_mm_pause();
+		}
+		void unlock() const
+		{
+			if constexpr ( atomic )
+				spinlock.clear( std::memory_order_release );
+		}
 
 		// Inserts the key into the list.
 		//
 		void emplace_front( key* k )
 		{
+			std::lock_guard _g( *this );
+
 			k->prev = nullptr;
 			k->next = head;
 			if ( head ) head->prev = k;
 			if ( !tail ) tail = k;
 			head = k;
+			size++;
 		}
 
 		// Inserts the key into the list.
 		//
 		void emplace_back( key* k )
 		{
+			std::lock_guard _g( *this );
+
 			k->prev = tail;
 			k->next = nullptr;
 			if ( tail ) tail->next = k;
 			if ( !head ) head = k;
 			tail = k;
+			size++;
 		}
 
 		// Erases the key from the list.
 		//
-		void erase( key* k )
+		void erase( key* k, bool inherit_lock = false )
 		{
+			if ( !inherit_lock ) lock();
+
 			if ( head == k ) head = k->next;
 			if ( tail == k ) tail = k->prev;
 			if ( k->prev ) k->prev->next = k->next;
 			if ( k->next ) k->next->prev = k->prev;
 			k->prev = nullptr;
 			k->next = nullptr;
+			size--;
+
+			unlock();
 		}
 
 		// Resets the list.
 		//
 		void reset()
 		{
+			std::lock_guard _g( *this );
+
 			head = nullptr;
 			tail = nullptr;
+			size = 0;
 		}
 		
 		// Pop front / back.
 		//
 		T* pop_front( member_reference_t<T, key> ref )
 		{
+			std::lock_guard _g( *this );
+
 			if ( key* entry = head )
 			{
 				T* value = entry->get( ref );
-				head = entry->next;
-				head->prev = nullptr;
-				entry->next = nullptr;
-				entry->prev = nullptr;
+				erase( entry, true );
 				return value;
 			}
 			return nullptr;
 		}
 		T* pop_back( member_reference_t<T, key> ref )
 		{
+			std::lock_guard _g( *this );
+
 			if ( key* entry = tail )
 			{
 				T* value = entry->get( ref );
-				tail = entry->prev;
-				tail->next = nullptr;
-				entry->next = nullptr;
-				entry->prev = nullptr;
+				erase( entry, true );
 				return value;
 			}
 			return nullptr;
 		}
 	};
+
+	// Declare aliases using base class.
+	//
+	template<typename T> using detached_queue =        base_detached_queue<T, false>;
+	template<typename T> using atomic_detached_queue = base_detached_queue<T, true>;
 };
