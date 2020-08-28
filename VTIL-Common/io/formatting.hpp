7ebdc76a1c499fa6242418a5f27ab57abcb22a9d
Can Bölük <can.boluk89@gmail.com>
2020-08-28 07:59:06 +0200

    Fixed Tuple-like for format::table_view, added table limits.

--- VTIL-Common/io/formatting.hpp
+++ VTIL-Common/io/formatting.hpp
@@ -327,10 +327,10 @@ namespace vtil::format
 
 	// Formats the integer into a signed hexadecimal.
 	//
-	template<typename T, std::enable_if_t<std::is_integral_v<std::remove_cvref_t<T>>, int> = 0>
-	static std::string hex( T&& value )
+	template<Integral T>
+	static std::string hex( T value )
 	{
-		if constexpr ( !std::is_signed_v<std::remove_cvref_t<T>> )
+		if constexpr ( !std::is_signed_v<T> )
 		{
 			return str( "0x%llx", value );
 		}
@@ -357,14 +357,16 @@ namespace vtil::format
         char horizontal_delimiter = '-';
         size_t left_pad =           0;
         size_t right_pad =          0;
+		size_t max_entries =        std::numeric_limits<size_t>::max();
+		size_t field_max_length =   std::numeric_limits<size_t>::max();
     };
 
     // Declare table structure, data source container must hold a tuple with
     // every element being string convertible by ::as_string.
     //
-    template<Iterable C> requires( Specialization<std::tuple, iterator_value_type_t<C>> && 
+    template<Iterable C> requires( Tuple<iterator_value_type_t<C>> && 
                                    StringConvertible<C> )
-    struct table
+    struct table_view
     {
         // Required typedefs.
         //
@@ -374,23 +376,24 @@ namespace vtil::format
         //
         static constexpr size_t field_count = std::tuple_size_v<iterator_value_type_t<C>>;
         
-        // Takes a data source and a list of labels.
+        // Takes a data source, a list of labels, and optionally rendering configuration.
         //
         C&& data_source;
-        std::array<std::string_view, field_count> labels;
-
-        constexpr table( C&& data_source, std::array<std::string_view, field_count> labels )
-            : data_source( std::forward<C>( data_source ) ), labels( std::move( labels ) ) {}
+		table_rendering_configuration config;
+		std::array<std::string_view, field_count> labels;
+		
+		constexpr table_view( C&& data_source, std::array<std::string_view, field_count> labels, table_rendering_configuration config = {} )
+            : data_source( std::forward<C>( data_source ) ), labels( std::move( labels ) ), config( std::move( config ) ) {}
 
         // Declare string conversion.
         //
-        std::string to_string( table_rendering_configuration config = {} ) const
+        std::string to_string() const
         {
             // Determine entry count.
             //
             const size_t entry_count = std::size( data_source );
 
-            // Convert fields in each entry into string.
+            // Convert fields in each entry into string and resize if over limit.
             //
             std::vector<std::array<std::string, field_count>> string_entries;
 			string_entries.reserve( entry_count );
@@ -401,8 +404,21 @@ namespace vtil::format
 				make_constant_series<field_count>( [ & ] ( auto tag )
 				{
 					auto at = []( auto&& x ) -> auto& { return std::get<decltype( tag )::value>( x ); };
-					output[ decltype( tag )::value ] = as_string( at( *eit ) );
+					std::string& str = output[ decltype( tag )::value ];
+
+					str = as_string( at( *eit ) );
+					if ( str.length() > config.field_max_length )
+					{
+						str.resize( config.field_max_length );
+						if ( config.field_max_length > 3 )
+							std::fill_n( str.end() - 3, 3, '.' );
+					}
 				} );
+
+				// Break if limit reached.
+				//
+				if ( string_entries.size() > config.max_entries )
+					break;
 			}
 
             // Determine field lengths.
@@ -418,11 +434,15 @@ namespace vtil::format
 
             // Allocate a buffer for the output.
             //
+			const bool table_overflow = string_entries.size() != entry_count;
             const size_t line_length = 
                 /* field data */ std::accumulate( field_lengths.begin(), field_lengths.end(), 0ull ) + 
                 /* delimiters */ 2 + field_count * 3 - 1 +
                 /* new line   */ 1;
-            const size_t line_count = entry_count + /*labels*/ 1 + /*delimiters*/ 3;
+            const size_t line_count = 
+				/* active entries */ string_entries.size() + 
+				/* labels         */ 1 + 
+				/* delimiters     */ 3 + table_overflow;
             
             std::string result( line_count * ( line_length + config.left_pad + config.right_pad ), '\0' );
             
@@ -433,7 +453,7 @@ namespace vtil::format
             auto write_n = [ & ] ( const std::string_view& v ) { iterator = std::copy( v.begin(), v.end(), iterator ); };
             auto fill =    [ & ] ( char c, size_t n )          { iterator = std::fill_n( iterator, n, c ); };
             auto begl =    [ & ] ()                            { fill( ' ', config.left_pad ); };
-            auto endl =    [ & ] () mutable                    { fill( ' ', config.right_pad ); *( iterator - 1 ) = '\n'; };
+            auto rendl =   [ & ] ()                            { fill( ' ', config.right_pad ); *( iterator - 1 ) = '\n'; };
 
             // Declare helpers for writing lines.
             //
@@ -441,7 +461,7 @@ namespace vtil::format
             {
                 begl();
                 fill( config.horizontal_delimiter, line_length );
-                endl();
+                rendl();
             };
             auto write_fields = [ & ] ( const auto& fields )
             {
@@ -455,7 +475,7 @@ namespace vtil::format
                         fill( ' ', end_real - iterator );
                     write( ' ', config.vertical_delimiter, ' ' );
                 }
-                endl();
+                rendl();
             };
             auto write_label_delim = [ & ] ()
             {
@@ -466,8 +486,19 @@ namespace vtil::format
                     fill( config.horizontal_delimiter, field_len );
                     write( config.horizontal_delimiter, config.vertical_delimiter, config.horizontal_delimiter );
                 }
-                endl();
+                rendl();
             };
+			auto write_overflow_delim = [ & ] ()
+			{
+				if ( table_overflow )
+				{
+					begl();
+					write( config.vertical_delimiter, ' ', '.', '.', '.' );
+					fill( ' ', line_length - 7 );
+					write( config.vertical_delimiter, ' ' );
+					rendl();
+				}
+			};
 
             // Format the whole table and return the result.
             //
@@ -476,6 +507,7 @@ namespace vtil::format
             write_label_delim();
             for( auto& fields : string_entries )
                 write_fields( fields );
+			write_overflow_delim();
             write_table_limit();
             return result;
         }
@@ -483,8 +515,8 @@ namespace vtil::format
 
     // Declare deduction guide.
     //
-    template<typename C>
-    table( C&&, std::initializer_list<std::string_view> )->table<C>;
+    template<typename C> table_view( C&&, std::initializer_list<std::string_view> )->table_view<C>;
+	template<typename C> table_view( C&&, std::initializer_list<std::string_view>, table_rendering_configuration )->table_view<C>;
 };
 #undef HAS_RTTI
 
