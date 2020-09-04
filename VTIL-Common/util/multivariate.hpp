b3f47b1d027df1560431e63d3b8dadf3beb72c42
Can Bölük <can.boluk89@gmail.com>
2020-09-04 20:32:57 +0200

    Changed the way updatable context works.

--- VTIL-Common/util/multivariate.hpp
+++ VTIL-Common/util/multivariate.hpp
@@ -28,6 +28,7 @@
 #pragma once
 #include <map>
 #include <mutex>
+#include <shared_mutex>
 #include "variant.hpp"
 #include "lt_typeid.hpp"
 #include "type_helpers.hpp"
@@ -35,15 +36,66 @@
 
 namespace vtil
 {
-	namespace impl
-	{
-		template<typename C, typename T>
-		concept HasContext = requires( T v, C* p ) { p = &v.context; };
-	};
+	template<typename C, typename T>
+	concept ContextSynchronizable = requires( T v, C * p ) { p = &v.context; v.epoch; };
 
-	// If context type inherits from this type, the result of [T& T::update( const owner* )] will be returned instead of T&.
+	// Synchronized context with an update callback that gets invoked if the previous value was outdated.
 	//
-	struct mv_updatable_tag {};
+	struct synchronized_context_tag {};
+	template<typename owner>
+	struct synchronized_context : synchronized_context_tag
+	{
+		// Lock for the update mechanism.
+		//
+		relaxed<std::mutex> update_lock;
+
+		// Timestamp of the last update.
+		//
+		using stamp_t = decltype( owner::epoch );
+		mutable relaxed_atomic<stamp_t> prev_epoch = {};
+
+		// Pointer to the last owner.
+		//
+		mutable relaxed_atomic<const owner*> prev_owner = nullptr;
+
+		// Implemented by parent, should update the context.
+		//
+		virtual void update( const owner* ) = 0;
+
+		// Checks if the current context is updated for the given owner.
+		//
+		bool is_updated( const owner* p ) const
+		{
+			return p == prev_owner && p->epoch == prev_epoch;
+		}
+
+		// Returns a self-reference after making sure context is updated.
+		//
+		void update_if( const owner* p )
+		{
+			// If context is not updated:
+			//
+			if ( !is_updated( p ) )
+			{
+				// Acquire lock, if still not updated, invoke ::update and change the epoch&owner.
+				//
+				std::lock_guard _g( update_lock );
+				if ( !is_updated( p ) )
+				{
+					update( p );
+					prev_owner = p; 
+					prev_epoch = p->epoch;
+				}
+			}
+		}
+
+		// Marks the context dirty.
+		//
+		void mark_dirty() const
+		{
+			prev_owner = nullptr;
+		}
+	};
 
 	// Multivariates store multiple types in a non-template type, mainly to be used by
 	// optimizers to store arbitrary per-block / per-instruction data at the respective 
@@ -52,7 +104,7 @@ namespace vtil
 	template<typename owner>
 	struct multivariate
 	{
-		mutable relaxed<std::mutex> mtx;
+		mutable relaxed<std::shared_mutex> mtx;
 		mutable std::unordered_map<size_t, variant> database;
 
 		// Default copy/move/construct.
@@ -65,11 +117,14 @@ namespace vtil
 
 		// Purges the object of the given type from the store.
 		//
-		template<typename T>
+		template<typename T = void>
 		void purge() const
 		{
-			std::lock_guard _g{ mtx };
-			database.erase( lt_typeid_v<T> );
+			std::unique_lock _g{ mtx };
+			if constexpr ( std::is_void_v<T> )
+				database.clear();
+			else
+				database.erase( lt_typeid_v<T> );
 		}
 
 		// Checks if we have the type in the store.
@@ -77,31 +132,60 @@ namespace vtil
 		template<typename T>
 		bool has() const
 		{
-			std::lock_guard _g{ mtx };
+			std::shared_lock _g{ mtx };
 			return database.contains( lt_typeid_v<T> );
 		}
 
 		// Getter of the types.
 		//
 		template<typename T>
+		T& get_raw() const
+		{
+			// Acquire shared lock and search for the value in the database:
+			//
+			T* value;
+			mtx.lock_shared();
+			auto it = database.find( lt_typeid_v<T> );
+
+			// If value does not exist:
+			//
+			if ( it == database.end() )
+			{
+				// Upgrade to a unique lock, emplace a new value and unlock.
+				//
+				mtx.unlock_shared();
+				mtx.lock();
+				auto [it, inserted] = database.emplace( lt_typeid_v<T>, make_default<T>() );
+				value = &it->second.template get<T>();
+				mtx.unlock();
+			}
+			else
+			{
+				// Get a reference and unlock.
+				//
+				value = &it->second.template get<T>();
+				mtx.unlock_shared();
+			}
+			return *value;
+		}
+		template<typename T>
 		T& get() const
 		{
-			// Acquire the database lock and check for existance.
+			// If synchronized:
 			//
-			std::lock_guard _g{ mtx };
-			variant& var = database[ lt_typeid_v<T> ];
+			T& ref = get_raw<T>();
+			if constexpr ( std::is_base_of_v<synchronized_context_tag, T> )
+			{
+				// Assert synchronizable validity.
+				//
+				static_assert( std::is_base_of_v<synchronized_context<owner>, T> 
+							   && ContextSynchronizable<multivariate<owner>, owner>, "Invalid context-synchronizable." );
 
-			// If not constructed yet:
-			//
-			if ( !var ) var = T();
-
-			// Return the reference.
-			//
-			T& ref = var.get<T>();
-			if constexpr ( std::is_base_of_v<mv_updatable_tag, T> && impl::HasContext<multivariate<owner>, owner> )
-				return ref.update( ptr_at<owner>( this, -make_offset( &owner::context ) ) );
-			else
-				return ref;
+				// Updated context if required.
+				//
+				ref.update_if( ptr_at<owner>( this, -make_offset( &owner::context ) ) );
+			}
+			return ref;
 		}
 
 		// Allows for convinient use of the type in the format of:
