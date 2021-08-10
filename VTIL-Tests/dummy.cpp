37b436a58405e960051e63d5e92c9d272ced493a
windy <19060@qq.com>
2021-08-10 20:15:09 +0800

    x86 Test OK

--- VTIL-Tests/dummy.cpp
+++ VTIL-Tests/dummy.cpp
@@ -9,3 +9,43 @@ DOCTEST_TEST_CASE("dummy")
 	vtil::debug::dump(block);
 	CHECK(1 == 1);
 }
+
+DOCTEST_TEST_CASE("Simplification")
+{
+    auto block = vtil::basic_block::begin(0x1337);
+    vtil::register_desc eax(vtil::register_physical, X86_REG_EAX, 32, 0);
+
+    block->mov(eax, 0);
+
+    for (int i = 0; i < 2; ++i)
+    {
+        block->add(eax, 13);
+        block->nop();
+        block->sub(eax, 12);
+        block->nop();
+        block->add(eax, 14);
+        block->mov(eax, eax);
+        block->sub(eax, eax);
+        block->bxor(eax, i);
+        block->push(eax);
+    }
+
+    block->vpinr(eax);  // pin register eax as read so it doesn't get optimized away
+    block->vexit(0ull); // marks the end of a basic_block
+
+    vtil::logger::log(":: Before:\n");
+    vtil::debug::dump(block->owner);
+
+    vtil::logger::log("\n");
+
+    vtil::optimizer::apply_each<
+        vtil::optimizer::profile_pass,
+        vtil::optimizer::collective_cross_pass
+    >{}(block->owner);      // executes all optimization passes
+
+    vtil::logger::log("\n");
+
+    vtil::logger::log(":: After:\n");
+    vtil::debug::dump(block->owner);
+    CHECK(1 == 1);
+}
